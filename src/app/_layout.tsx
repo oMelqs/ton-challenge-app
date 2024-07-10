0dcbedef1adb5c76968fb8734b1a7a52be59f02a
Melquisedeque Linhares <melquisedeque.linhares@gmail.com>
2024-07-09 21:55:52 -0300

    feat: add headers and navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,46 @@
-import {Stack} from 'expo-router/stack'
+import React from 'react'
+import {Feather} from '@expo/vector-icons'
+import {Link, Stack} from 'expo-router'
+
 import '../styles/global.css'
+
 export default function Layout() {
-  return <Stack />
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: 'Produtos',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <Link href="/cart">
+              <Feather name="shopping-cart" size={24} color="white" />
+            </Link>
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name="cart"
+        options={{
+          headerTitle: 'Carrinho de Compras',
+          headerStyle: {
+            backgroundColor: '#4CAF50',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerBackVisible: true,
+          headerBackTitleVisible: false,
+        }}
+      />
+    </Stack>
+  )
 }
--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,5 +1,5 @@
 import {View} from 'react-native'
 
-export default function Home() {
+export default function cart() {
   return <View></View>
 }
