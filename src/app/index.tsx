c9f5f9ac37580e38e8738f714cf1f277a664cd5b
Melquisedeque Linhares <melquisedeque.linhares@gmail.com>
2024-07-10 16:53:26 -0300

    feat: add error treatment

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import {View, FlatList} from 'react-native'
+import {View, FlatList, Text} from 'react-native'
 import {CardProduct} from '@/components/Product/CardProduct'
 import {useGetProducts} from '@/hooks/products/useGetProducts'
 import {tProduct} from '@/types/product'
 import SkeletonCardProduct from '@/components/Product/CardProduct/Skeleton'
+import {Entypo} from '@expo/vector-icons'
 
 export default function Home() {
-  const {data, isLoading} = useGetProducts()
+  const {data, isLoading, isError} = useGetProducts()
   const renderItem = ({item}: {item: tProduct}) => (
     <CardProduct key={item.id} product={item} />
   )
@@ -21,6 +22,32 @@ export default function Home() {
           numColumns={2}
           contentContainerStyle={{paddingBottom: 10}}
         />
+      </View>
+    )
+  }
+
+  if (isError) {
+    return (
+      <View className="flex-1 bg-white justify-center items-center">
+        <View className="pb-32">
+          <Entypo name="emoji-sad" size={32} color="red" className="pb-4" />
+          <Text className="font-bold text-4xl pb-4">Ops...</Text>
+          <Text className="text-xl">Something went wrong!</Text>
+          <Text className="text-xl">Try again later.</Text>
+        </View>
+      </View>
+    )
+  }
+
+  if (data && data.length === 0) {
+    return (
+      <View className="flex-1 bg-white justify-center items-center">
+        <View className="pb-32">
+          <Entypo name="emoji-sad" size={32} color="red" className="pb-4" />
+          <Text className="font-bold text-4xl pb-4">Ops...</Text>
+          <Text className="text-xl">No products found!</Text>
+          <Text className="text-xl">Try again later.</Text>
+        </View>
       </View>
     )
   }
--- src/hooks/products/useGetProducts.ts
+++ src/hooks/products/useGetProducts.ts
@@ -1,7 +1,26 @@
-import {useQuery} from 'react-query'
-export const useGetProducts = () => {
-  return useQuery('products', async () => {
-    const response = await fetch('https://fakestoreapi.com/products')
-    return response.json()
-  })
+import {useQuery, UseQueryResult} from 'react-query'
+import {tProduct} from '@/types/product'
+
+export const useGetProducts = (): UseQueryResult<tProduct[], Error> => {
+  return useQuery<tProduct[], Error>(
+    'products',
+    async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      } catch (error: unknown) {
+        throw new Error(`Fetching products failed`)
+      }
+    },
+    {
+      onError: (error: Error) => {
+        // Log the error or handle it as needed
+        console.error('Error fetching products:', error)
+        // You can also show a user-friendly message or perform other actions
+      },
+    },
+  )
 }
